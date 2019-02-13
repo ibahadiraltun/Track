582398868f658c1442ba16c417f26c9df78f7697
Bahadir Altun <ibahadiraltun@Bahadirs-MacBook-Air.local>
2019-02-13 23:16:32 +0300

    Save location and acceleration values for 5 mins intervals

--- Track/ViewController.swift
+++ Track/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import Foundation
 import CoreLocation
 import CoreMotion
 import SwiftProtobuf
+
+// Acceleration class to keep current acceleration and time to compare with other locations
+class Acceleration {
+    
+    var accData: ProtoFiles_AccData = ProtoFiles_AccData()
+    var time = Double()
+    
+    init(_accData: ProtoFiles_AccData, _time: Double) {
+        accData = _accData
+        time = _time
+    }
+    
+}
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
@@ -18,6 +32,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager: CLLocationManager = CLLocationManager()
     let motionManager: CMMotionManager = CMMotionManager()
+    
+    // timer to save data every 10 seconds
+    var timer = Timer()
+    var currentTime = 0.0
+    let startTime = Date().timeIntervalSinceReferenceDate
+
+    var unitFile = ProtoFiles_UnitFile()
+    
+    var accelerations: [Acceleration] = [Acceleration]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,17 +59,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         // start tracking
         
+        // initialize unitFile
+        unitFile = ProtoFiles_UnitFile()
+        
         // initialize location manager
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         
         // initialize motion manager i.e acceleration
-        motionManager.accelerometerUpdateInterval = 0.5
-        motionManager.startAccelerometerUpdates()
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let _data = data {
+                
+                // to keep acceleration data between current and previous locations
+                
+                var accData = ProtoFiles_AccData()
+                accData.timestamp = self.currentTime
+                accData.x = Float((_data.acceleration.x))
+                accData.y = Float((_data.acceleration.y))
+                accData.z = Float((_data.acceleration.z))
+
+                self.accelerations.append(Acceleration(_accData: accData, _time: self.currentTime))
+            }
+        }
         
-//        print(motionManager.accelerometerData)
-//        print(locationManager.location)
-//        print(locationManager.desiredAccuracy)
+        // initialize timer to repeat every 3 seconds
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(ViewController.saveData), userInfo: nil, repeats: true)
         
     }
    
@@ -55,22 +93,72 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         startButton.isEnabled = true
         stopButton.isEnabled = false
         
-        // stop tracking
+        // stop timing
+        timer.invalidate()
         
+        // stop tracking
         locationManager.stopUpdatingLocation()
         motionManager.stopAccelerometerUpdates()
         
+        saveUnitFile()
+        
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    @objc func saveData() {
         
-        for curLocation in locations {
-            
-        //    print("Location -> \(index): + \(curLocation)")
-            
+        currentTime = currentTime + 3.0
+        
+        print("Next Save -- > \(currentTime)")
+        
+        let currentLoc = locationManager.location
+        var locData = ProtoFiles_LocData()
+
+        locData.timestamp = currentTime
+        locData.latitude = Float((currentLoc?.coordinate.latitude)!)
+        locData.longitude = Float((currentLoc?.coordinate.longitude)!)
+        locData.accuracy = Float(locationManager.desiredAccuracy)
+        locData.speed = Float((currentLoc?.speed)!)
+        
+        for each in accelerations {
+            locData.accData.append(each.accData)
+        }
+        
+        // we only need to keep accelerations between current and previous locations, so we can clear [accelerations] to use memory better
+        accelerations = [Acceleration]()
+        
+        print(locData)
+     
+        unitFile.locData.append(locData)
+        
+    }
+    
+    func saveUnitFile() {
+        
+        let endTime = Date().timeIntervalSinceReferenceDate
+
+        unitFile.driverID = UInt64((UIDevice.current.identifierForVendor?.hashValue)!)
+        unitFile.timezoneoffset = Int32(TimeZone.current.secondsFromGMT())
+        unitFile.startTime = startTime
+        unitFile.endTime = endTime
+        
+        print(unitFile)
+        var binaryData: Data = Data()
+        
+        do {
+            binaryData = try unitFile.serializedData()
+        } catch {
+            print("Some error occured while serializing data")
+        }
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let dataURL = URL(fileURLWithPath: "TrackingData", relativeTo: paths[0])
+        
+        do {
+            try binaryData.write(to: dataURL)
+        } catch {
+            print("Some error occured while saving data")
         }
         
     }
     
 }
-
