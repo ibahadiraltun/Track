a38216cd25483d85ffbf2af175ae9fb80893a7e6
Bahadir Altun <ibahadiraltun@Bahadirs-MacBook-Air.local>
2019-02-13 19:58:13 +0300

    Subscribe to location and acceleration sensors

--- Track/ViewController.swift
+++ Track/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager: CLLocationManager = CLLocationManager()
+    let motionManager: CMMotionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        
     }
 
-
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        
+        // start tracking
+        
+        // initialize location manager
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        // initialize motion manager i.e acceleration
+        motionManager.accelerometerUpdateInterval = 0.1
+        
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (_data, error) in
+            if let data = _data {
+                print(data)
+            }
+        }
+        
+        
+    }
+   
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        
+        // stop tracking
+        
+        locationManager.stopUpdatingLocation()
+        motionManager.stopAccelerometerUpdates()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        for curLocation in locations {
+            
+            print("Location -> \(index): + \(curLocation)")
+            
+        }
+        
+    }
+    
 }
 
